da1a4b370b0d661bcaba604baac41b71e450d24e
顾安 <1773966022@qq.com>
2020-01-11 11:05:43 +0800

    test

---
+++ src/com/wu/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.wu.test;
+
+/**
+ * @author 武爸爸
+ * @create 2020-01-11 10:58
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
