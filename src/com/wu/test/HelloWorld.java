36b990be9f7ea1906701e05acf357e56f7659ec2
顾安 <1773966022@qq.com>
2020-01-11 11:10:46 +0800

    test

--- src/com/wu/test/HelloWorld.java
+++ src/com/wu/test/HelloWorld.java
@@ -6,6 +6,6 @@ package com.wu.test;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 99");
     }
 }
